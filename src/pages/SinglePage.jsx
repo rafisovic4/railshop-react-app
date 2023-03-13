bcda721bc4e4cc43c0d35d1559f9ac0f6523d376
Ranis <Oskar_1223@mail.ru>
2023-03-13 12:48:36 +0300

    Finally version

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import image from "../assets/image/slider-image.png"
+import { NavLink, useParams } from "react-router-dom";
+
+import PRODUCTS from "../data/PRODUCTS";
+import formatMoney from "../utils/formatMoney";
 
 const SinglePage = () => {
-    const [service, setService] = useState({});
+    const params = useParams();
+
+    const tovar = PRODUCTS.filter(item => item.id == params.id)
 
     return (
         <section className="one-tovar">
-            <div className="osnova__one-tovar">
+            <div className="osnova__one-tovar" key={tovar[0].id}>
                 <div className="leftside-image__one-tovar">
-                    <img src={image} alt="" className="single-page__image" />
+                    <img src={tovar[0].preview} alt="" className="single-page__image" />
                 </div>
                 <div className="rightside-text__one-tovar">
-                    <div className="single--name">Naimenovanie</div>
+                    <div className="single--name">{tovar[0].name}</div>
                     <div className="single--description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui eum expedita, facilis iusto, sunt soluta nam minima hic ipsa, alias quod inventore. Quod alias, quas cum aliquid esse officiis vero.</div>
-                    <div className="single--price">1150 руб.</div>
+                    <div className="single--price">{formatMoney(tovar[0].price)}</div>
                     <div className="single--button">
                         <NavLink to="/"><button className="form-button">Назад</button></NavLink>
                     </div>
